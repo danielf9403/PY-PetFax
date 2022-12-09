6e7935ad651af48b1d29e91bf3472ece06e38053
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-12-08 22:24:53 -0500

    :P

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,16 +1,24 @@
-from flask import ( Blueprint, render_template, request ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models
+
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-@bp.route('/new')
-def new(): 
-    return render_template('facts/new.html')
 
-
-@bp.route('/', methods=['GET', 'POST']) 
+@bp.route('/', methods=['GET', 'POST'])
 def index(): 
     if request.method == 'POST':
-       print(request.form)
-       return redirect('/facts')
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+
+results = models.Fact.query.all()
+for results in results:
+    print(results)
     
-    return render_template('facts/index.html')
+    return render_template('facts/index.html', facts=results)
